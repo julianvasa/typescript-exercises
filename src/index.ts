e606a00ed7b261122b2c82cafeb642ed43a57764
Julian Vasa <julian@k15t.com>
2020-11-25 20:47:31 +0100

    Variable types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello !');
+console.log('Hello World!')
+
+const myString = "Hello brave new world"
+// myString = "Trying to modify"; // Cannot modify const var
+console.log(myString);
+
+let myNumber = 4 // Inferred the type of the variable from the assigned value
+myNumber = 10
+console.log(myNumber)
+myNumber = 0xf03 // number can be int, float, hex 
+console.log(myNumber)
+
+let myAnyVariable:unknown = "String" // or type = any
+console.log(myAnyVariable);
+myAnyVariable = 3
+console.log(myAnyVariable);
